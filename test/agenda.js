a88ff16eff24ebfd684509cedb3572372c8e95f6
Liam Don <liamdon@gmail.com>
2015-09-29 15:18:49 -0700

    Restore original tests, now with mongodb-native driver

--- test/agenda.js
+++ test/agenda.js
@@ -5,23 +5,30 @@ var mongoHost = process.env.MONGODB_HOST || 'localhost',
 
 var expect = require('expect.js'),
     path = require('path'),
-    Agenda = require( path.join('..', 'index.js') );
+    Agenda = require( path.join('..', 'index.js') ),
+    Job = require( path.join('..', 'lib', 'job.js') );
 
 var MongoClient = require('mongodb').MongoClient;
 var mongo = null;
 
-// create agenda instance
+// create agenda instances
 var jobs = new Agenda({
-  processEvery: '0.1 seconds',
   db: {
     address: mongoCfg
   }
 });
 
-var jobType = 'do werk';
-jobs.define(jobType, function(job, done) {
-  done();
-});
+function clearJobs(done) {
+  mongo.collection('agendaJobs').remove({}, done);
+}
+
+// Slow timeouts for travis
+var jobTimeout = process.env.TRAVIS ? 1000 : 300;
+
+
+var jobType       = 'do work';
+var jobProcessor  = function(job) { };
+
 
 function failOnError(err) {
   if (err) {
@@ -29,68 +36,387 @@ function failOnError(err) {
   }
 }
 
-describe("mongo persistence", function() {
+describe("agenda", function() {
+
   before(function(done) {
     MongoClient.connect(mongoCfg, function( error, db ){
       mongo = db;
       setTimeout(function() {
-        done(error);
-      }, 750);
+        clearJobs(function() {
+          jobs.define('someJob', jobProcessor);
+          jobs.define('send email', jobProcessor);
+          jobs.define('some job', jobProcessor);
+          jobs.define(jobType, jobProcessor);
+          done();
+        });
+      }, 500);
     });
   });
 
   after(function(done) {
       setTimeout(function() {
         mongo.close(done);
-      }, 750);
+      }, 250);
   });
 
-  beforeEach(function(done) {
-    mongo.collection('agendaJobs').remove({}, function(err) {
-      failOnError(err);
-      jobs.start();
-      done();
+
+  describe('Agenda', function() {
+    it('sets a default processEvery', function() {
+      expect(jobs._processEvery).to.be(5000);
     });
-  });
 
-  afterEach(function(done) {
-    jobs.stop(done);
-  });
+    describe('configuration methods', function() {
+      it('sets the _db directly when passed as an option', function() {
+        var agenda = new Agenda({mongo: mongo});
+        expect(agenda._mdb.databaseName).to.equal('agenda-test');
+      });
+    });
 
-  it("saves scheduled job to db", function(done) {
-    jobs
-      .create(jobType, {})
-      .schedule('in 10 minutes')
-      .save(function(err) {
-        failOnError(err);
+    describe('configuration methods', function() {
 
-        mongo.collection('agendaJobs').find().toArray(function(err, arr) {
-          failOnError(err);
-          expect(arr).to.have.length(1);
-          done();
+      describe('mongo', function() {
+        it('sets the _db directly', function() {
+          var agenda = new Agenda();
+          agenda.mongo(mongo);
+          expect(agenda._mdb.databaseName).to.equal('agenda-test');
+        });
+
+        it('returns itself', function() {
+          var agenda = new Agenda();
+          expect(agenda.mongo(mongo)).to.be(agenda);
         });
       });
-  });
 
-  it("clears jobs after completion", function(done) {
-    jobs
-      .create(jobType, {})
-      .schedule('now')
-      .save(function(err) {
-        failOnError(err);
+      describe('name', function() {
+        it('sets the agenda name', function() {
+          jobs.name('test queue');
+          expect(jobs._name).to.be('test queue');
+        });
+        it('returns itself', function() {
+          expect(jobs.name('test queue')).to.be(jobs);
+        });
+      });
 
-        mongo.collection('agendaJobs').find().toArray(function(err, arr) {
-          failOnError(err);
-          expect(arr).to.have.length(1);
+      describe('processEvery', function() {
+        it('sets the processEvery time', function() {
+          jobs.processEvery('3 minutes');
+          expect(jobs._processEvery).to.be(180000);
+        });
+        it('returns itself', function() {
+          expect(jobs.processEvery('3 minutes')).to.be(jobs);
+        });
+      });
+      describe('maxConcurrency', function() {
+        it('sets the maxConcurrency', function() {
+          jobs.maxConcurrency(10);
+          expect(jobs._maxConcurrency).to.be(10);
+        });
+        it('returns itself', function() {
+          expect(jobs.maxConcurrency(10)).to.be(jobs);
+        });
+      });
+      describe('defaultConcurrency', function() {
+        it('sets the defaultConcurrency', function() {
+          jobs.defaultConcurrency(1);
+          expect(jobs._defaultConcurrency).to.be(1);
+        });
+        it('returns itself', function() {
+          expect(jobs.defaultConcurrency(5)).to.be(jobs);
+        });
+      });
+      describe('defaultLockLifetime', function(){
+        it('returns itself', function() {
+          expect(jobs.defaultLockLifetime(1000)).to.be(jobs);
+        });
+        it('sets the default lock lifetime', function(){
+          jobs.defaultLockLifetime(9999);
+          expect(jobs._defaultLockLifetime).to.be(9999);
+        });
+        it('is inherited by jobs', function(){
+          jobs.defaultLockLifetime(7777);
+          jobs.define('testDefaultLockLifetime', function(job, done){});
+          expect(jobs._definitions.testDefaultLockLifetime.lockLifetime).to.be(7777);
+        });
+      });
+    });
+
+    describe('job methods', function() {
+
+      describe('create', function() {
+        var job;
+        beforeEach(function() {
+          job = jobs.create('sendEmail', {to: 'some guy'});
         });
 
-        jobs.on('complete', function() {
-          mongo.collection('agendaJobs').find().toArray(function(err, arr) {
-            failOnError(err);
-            expect(arr).to.have.length(0);
-            done();
+        it('returns a job', function() {
+          expect(job).to.be.a(Job);
+        });
+        it('sets the name', function() {
+          expect(job.attrs.name).to.be('sendEmail');
+        });
+        it('sets the type', function() {
+          expect(job.attrs.type).to.be('normal');
+        });
+        it('sets the agenda', function() {
+          expect(job.agenda).to.be(jobs);
+        });
+        it('sets the data', function() {
+          expect(job.attrs.data).to.have.property('to', 'some guy');
+        });
+      });
+
+      describe('define', function() {
+
+        it('stores the definition for the job', function() {
+          expect(jobs._definitions.someJob).to.have.property('fn', jobProcessor);
+        });
+
+        it('sets the default concurrency for the job', function() {
+          expect(jobs._definitions.someJob).to.have.property('concurrency', 5);
+        });
+
+        it('sets the default priority for the job', function() {
+          expect(jobs._definitions.someJob).to.have.property('priority', 0);
+        });
+        it('takes concurrency option for the job', function() {
+          jobs.define('highPriority', {priority: 10}, jobProcessor);
+          expect(jobs._definitions.highPriority).to.have.property('priority', 10);
+        });
+      });
+
+      describe('every', function() {
+        describe('with a job name specified', function() {
+          it('returns a job', function() {
+            expect(jobs.every('5 minutes', 'send email')).to.be.a(Job);
+          });
+          it('sets the repeatEvery', function() {
+            expect(jobs.every('5 seconds', 'send email').attrs.repeatInterval).to.be('5 seconds');
+          });
+          it('sets the agenda', function() {
+            expect(jobs.every('5 seconds', 'send email').agenda).to.be(jobs);
+          });
+          it('should update a job that was previously scheduled with `every`', function(done) {
+            jobs.every(10, 'shouldBeSingleJob');
+            setTimeout(function() {
+              jobs.every(20, 'shouldBeSingleJob');
+            }, 10);
+
+            // Give the saves a little time to propagate
+            setTimeout(function() {
+              jobs.jobs({name: 'shouldBeSingleJob'}, function(err, res) {
+                expect(res).to.have.length(1);
+                done();
+              });
+            }, jobTimeout);
+
+          });
+        });
+        describe('with array of names specified', function () {
+          it('returns array of jobs', function () {
+            expect(jobs.every('5 minutes', ['send email', 'some job'])).to.be.an('array');
+          });
+        });
+        after(clearJobs);
+      });
+
+      describe('schedule', function() {
+        describe('with a job name specified', function() {
+          it('returns a job', function() {
+            expect(jobs.schedule('in 5 minutes', 'send email')).to.be.a(Job);
+          });
+          it('sets the schedule', function() {
+            var fiveish = (new Date()).valueOf() + 250000;
+            expect(jobs.schedule('in 5 minutes', 'send email').attrs.nextRunAt.valueOf()).to.be.greaterThan(fiveish);
+          });
+        });
+        describe('with array of names specified', function () {
+          it('returns array of jobs', function () {
+            expect(jobs.schedule('5 minutes', ['send email', 'some job'])).to.be.an('array');
+          });
+        });
+        after(clearJobs);
+      });
+
+      describe('unique', function() {
+
+        describe('should demonstrate unique contraint', function(done) {
+
+          it('should create one job when unique matches', function(done) {
+            var time = new Date();
+            jobs.create('unique job', {type: 'active', userId: '123', 'other': true}).unique({'data.type': 'active', 'data.userId': '123', nextRunAt: time}).schedule(time).save(function(err, job) {
+             jobs.create('unique job', {type: 'active', userId: '123', 'other': false}).unique({'data.type': 'active', 'data.userId': '123', nextRunAt: time}).schedule(time).save(function(err, job) {
+                mongo.collection('agendaJobs').find({name: 'unique job'}).toArray(function(err, j) {
+                  expect(j).to.have.length(1);
+                  done();
+                });
+             });
+            });
+          });
+          after(clearJobs);
+
+        });
+
+        describe('should demonstrate non-unique contraint', function(done) {
+
+          it('should create two jobs when unique doesn\t match', function(done) {
+            var time = new Date(Date.now() + 1000*60*3);
+            var time2 = new Date(Date.now() + 1000*60*4);
+
+            jobs.create('unique job', {type: 'active', userId: '123', 'other': true}).unique({'data.type': 'active', 'data.userId': '123', nextRunAt: time}).schedule(time).save(function(err, job) {
+             jobs.create('unique job', {type: 'active', userId: '123', 'other': false}).unique({'data.type': 'active', 'data.userId': '123', nextRunAt: time2}).schedule(time).save(function(err, job) {
+                mongo.collection('agendaJobs').find({name: 'unique job'}).toArray(function(err, j) {
+                  expect(j).to.have.length(2);
+                  done();
+                });
+             });
+            });
+
+          });
+          after(clearJobs);
+
+        });
+
+      });
+
+      describe('now', function() {
+        it('returns a job', function() {
+          expect(jobs.now('send email')).to.be.a(Job);
+        });
+        it('sets the schedule', function() {
+          var now = new Date();
+          expect(jobs.now('send email').attrs.nextRunAt.valueOf()).to.be.greaterThan(now.valueOf() - 1);
+        });
+
+        it('runs the job immediately', function(done) {
+          jobs.define('immediateJob', function(job) {
+            expect(job.isRunning()).to.be(true);
+            jobs.stop(done);
+          });
+          jobs.now('immediateJob');
+          jobs.start();
+        });
+
+        after(clearJobs);
+      });
+
+      describe('jobs', function() {
+        it('returns jobs', function(done) {
+          var job = jobs.create('test');
+          job.save(function() {
+            jobs.jobs({}, function(err, c) {
+              expect(c.length).to.not.be(0);
+              expect(c[0]).to.be.a(Job);
+              clearJobs(done);
+            });
           });
         });
       });
+
+      describe('purge', function() {
+        it('removes all jobs without definitions', function(done) {
+          var job = jobs.create('no definition');
+          jobs.stop(function() {
+            job.save(function() {
+              jobs.jobs({name: 'no definition'}, function(err, j) {
+                if(err) return done(err);
+                expect(j).to.have.length(1);
+                jobs.purge(function(err) {
+                  if(err) return done(err);
+                  jobs.jobs({name: 'no definition'}, function(err, j) {
+                    if(err) return done(err);
+                    expect(j).to.have.length(0);
+                    done();
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+
+      describe('saveJob', function() {
+        it('persists job to the database', function(done) {
+          var job = jobs.create('someJob', {});
+          job.save(function(err, job) {
+            expect(job.attrs._id).to.be.ok();
+            clearJobs(done);
+          });
+        });
+      });
+    });
+
+    describe('cancel', function() {
+      beforeEach(function(done) {
+        var remaining = 3;
+        var checkDone = function(err) {
+          if(err) return done(err);
+          remaining--;
+          if(!remaining) {
+            done();
+          }
+        };
+        jobs.create('jobA').save(checkDone);
+        jobs.create('jobA', 'someData').save(checkDone);
+        jobs.create('jobB').save(checkDone);
+      });
+
+      afterEach(function(done) {
+        jobs._collection.remove({name: {$in: ['jobA', 'jobB']}}, function(err) {
+          if(err) return done(err);
+          done();
+        });
+      });
+
+      it('should cancel a job', function(done) {
+        jobs.jobs({name: 'jobA'}, function(err, j) {
+          if(err) return done(err);
+          expect(j).to.have.length(2);
+          jobs.cancel({name: 'jobA'}, function(err) {
+            if(err) return done(err);
+            jobs.jobs({name: 'jobA'}, function(err, j) {
+              if(err) return done(err);
+              expect(j).to.have.length(0);
+              done();
+            });
+          });
+        });
+      });
+
+      it('should cancel multiple jobs', function(done) {
+        jobs.jobs({name: {$in: ['jobA', 'jobB']}}, function(err, j) {
+          if(err) return done(err);
+          expect(j).to.have.length(3);
+          jobs.cancel({name: {$in: ['jobA', 'jobB']}}, function(err) {
+            if(err) return done(err);
+            jobs.jobs({name: {$in: ['jobA', 'jobB']}}, function(err, j) {
+              if(err) return done(err);
+              expect(j).to.have.length(0);
+              done();
+            });
+          });
+        });
+      });
+
+      it('should cancel jobs only if the data matches', function(done){
+        jobs.jobs({name: 'jobA', data: 'someData'}, function(err, j) {
+          if(err) return done(err);
+          expect(j).to.have.length(1);
+          jobs.cancel({name: 'jobA', data: 'someData'}, function(err) {
+            if(err) return done(err);
+            jobs.jobs({name: 'jobA', data: 'someData'}, function(err, j) {
+              if(err) return done(err);
+              expect(j).to.have.length(0);
+              jobs.jobs({name: 'jobA'}, function(err, j) {
+                if(err) return done(err);
+                expect(j).to.have.length(1);
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
   });
+
 });
+
