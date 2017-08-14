1f6b68ac789ca2e47f034b89716c72c22acddcf4
Alexis Tyler <xo@wvvw.me>
2017-08-14 23:33:58 +0930

    add agenda.defaultConcurrency test

---
+++ test/agenda/default-concurrency.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import {Agenda} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach.always(afterEach);
+
+test('returns itself', t => {
+  const {agenda} = t.context;
+
+  t.true(agenda.defaultConcurrency(5) instanceof Agenda);
+});
+
+test('sets the defaultConcurrency', t => {
+  const {agenda} = t.context;
+
+  agenda.defaultConcurrency(1);
+
+  t.is(agenda._defaultConcurrency, 1);
+});
