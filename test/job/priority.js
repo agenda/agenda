4ee8de4bf5393ce052bb6096cc234ccb64f0bb3a
Alexis Tyler <xo@wvvw.me>
2017-08-15 04:18:54 +0930

    add job.priority test

---
+++ test/job/priority.js
@@ -0,0 +1,28 @@
+import test from 'ava';
+import {Job} from '../../lib';
+import {beforeEach, afterEach} from '../helpers/job';
+
+test.beforeEach(beforeEach);
+test.afterEach(afterEach);
+
+test('returns the job', t => {
+  const {job} = t.context;
+
+  t.true(job.priority(50) instanceof Job);
+});
+
+test('sets the priority to a number', t => {
+  const {job} = t.context;
+
+  job.priority(10);
+
+  t.is(job.attrs.priority, 10);
+});
+
+test('parses written priorities', t => {
+  const {job} = t.context;
+
+  job.priority('high');
+
+  t.is(job.attrs.priority, 10);
+});
