2419ba47e59f9f083f0ae0bcb2cee922d2a3e431
Alexis Tyler <xo@wvvw.me>
2017-08-14 23:35:37 +0930

    add agenda.maxConcurrency test

---
+++ test/agenda/max-concurrency.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import {Agenda} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach.always(afterEach);
+
+test('returns itself', t => {
+  const {agenda} = t.context;
+
+  t.true(agenda.maxConcurrency(10) instanceof Agenda);
+});
+
+test('sets the maxConcurrency', t => {
+  const {agenda} = t.context;
+
+  agenda.maxConcurrency(10);
+
+  t.is(agenda._maxConcurrency, 10);
+});
