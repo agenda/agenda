d47f965e6af933e330b11f2ff39be412245fd6dd
Alexis Tyler <xo@wvvw.me>
2017-08-15 05:47:17 +0930

    add job.enable test

---
+++ test/job/enable.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import {Job} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach(afterEach);
+
+test('returns the job', t => {
+  const job = new Job({disabled: true});
+
+  t.true(job.enable() instanceof Job);
+});
+
+test('sets disabled to false on the job', t => {
+  const job = new Job({disabled: true});
+
+  job.enable();
+
+  t.false(job.attrs.disabled);
+});
