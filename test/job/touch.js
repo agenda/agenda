85a8a79d9cb1da4504c3ba2b6acc50c88c858168
Alexis Tyler <xo@wvvw.me>
2017-08-15 06:13:47 +0930

    add job.touch test

---
+++ test/job/touch.js
@@ -0,0 +1,29 @@
+import test from 'ava';
+import delay from 'delay';
+import {Job} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach(afterEach);
+
+test('returns the job', t => {
+  const {job} = t.context;
+
+  t.is(job.touch(), undefined);
+});
+
+test('extends the lock lifetime', async t => {
+  const {agenda} = t.context;
+  const lockedAt = new Date();
+  const job = new Job({agenda, name: 'some job', lockedAt});
+
+  job.save = cb => cb();
+
+  await delay(2);
+
+  await new Promise(resolve => {
+    job.touch(() => resolve());
+  });
+
+  t.true(job.attrs.lockedAt > lockedAt);
+});
