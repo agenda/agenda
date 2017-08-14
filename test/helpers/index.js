9a83afced5a9581cbbb64fd41832512639ecc783
Alexis Tyler <xo@wvvw.me>
2017-08-14 23:32:27 +0930

    add helper file for ava

---
+++ test/helpers/index.js
@@ -0,0 +1,56 @@
+import uuidv4 from 'uuid/v4';
+import {MongoClient} from 'mongodb';
+import Agenda from '../../';
+
+const mongoHost = process.env.MONGODB_HOST || 'localhost';
+const mongoPort = process.env.MONGODB_PORT || '27017';
+const jobProcessor = () => {};
+
+const beforeEach = async t => {
+  const databaseName = uuidv4();
+  const mongoCfg = `mongodb://${mongoHost}:${mongoPort}/${databaseName}`;
+  const agenda = new Agenda({db: {address: mongoCfg}});
+  const mongo = await MongoClient.connect(mongoCfg);
+
+  return new Promise(resolve => {
+    agenda.on('ready', () => {
+      agenda.define('someJob', jobProcessor);
+      agenda.define('send email', jobProcessor);
+      agenda.define('some job', jobProcessor);
+      agenda.define('do work', jobProcessor);
+
+      // @NOTE: Anyting that needs to be access via t.context
+      //        should be added here and only here.
+      t.context = Object.assign({
+        agenda,
+        mongo,
+        mongoHost,
+        mongoPort,
+        mongoCfg,
+        databaseName,
+        jobProcessor
+      }, t.context);
+
+      resolve();
+    });
+  });
+};
+
+const afterEach = t => {
+  const {agenda, mongo} = t.context;
+
+  return new Promise(resolve => {
+    agenda.stop(() => {
+      mongo.collection('agendaJobs').remove({}, () => {
+        mongo.close(() => {
+          agenda._mdb.close(resolve);
+        });
+      });
+    });
+  });
+};
+
+export {
+  beforeEach,
+  afterEach
+};
