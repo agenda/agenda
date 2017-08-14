85224953710a2959de586b565698a5892538d813
Alexis Tyler <xo@wvvw.me>
2017-08-14 23:35:52 +0930

    add agenda.processEvery test

---
+++ test/agenda/process-every.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import {Agenda} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach.always(afterEach);
+
+test('returns itself', t => {
+  const {agenda} = t.context;
+
+  t.true(agenda.processEvery('3 minutes') instanceof Agenda);
+});
+
+test('sets the processEvery time', t => {
+  const {agenda} = t.context;
+
+  agenda.processEvery('3 minutes');
+
+  t.is(agenda._processEvery, 180000);
+});
