a3b552cc6dae9e0f4209f844c48c5cbcc66b4d03
Alexis Tyler <xo@wvvw.me>
2017-08-14 23:33:46 +0930

    add agenda.create test

---
+++ test/agenda/create.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import {Job} from '../../lib';
+import {beforeEach, afterEach} from '../helpers';
+
+test.beforeEach(beforeEach);
+test.afterEach.always(afterEach);
+
+test('returns a job', t => {
+  const {agenda} = t.context;
+
+  t.true(agenda.create('sendEmail', {to: 'some guy'}) instanceof Job);
+});
+
+test('creates a job', t => {
+  const {agenda} = t.context;
+  const job = agenda.create('sendEmail', {to: 'some guy'});
+
+  t.is(job.attrs.name, 'sendEmail');
+  t.is(job.attrs.type, 'normal');
+  t.deepEqual(job.agenda, agenda);
+  t.is(job.attrs.data.to, 'some guy');
+});
