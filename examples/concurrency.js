b774785e0a05eb5a4a0f41043db908bea79a910c
Dan Dascalescu <ddascalescu+github@gmail.com>
2019-03-28 19:36:29 -0500

    Add two more examples

--- examples/concurrency.js
+++ examples/concurrency.js
@@ -19,15 +19,29 @@ const agenda = new Agenda({
   },
 });
 
-let jobRuncount = 1;
+let jobRunCount = 1;
 agenda.define('long-running job', {
   lockLifetime: 5 * 1000,  // max amount of time the job should take
   concurrency: 3,  // max number of job instances to run at the same time
 }, async (job, done) => {
-  const thisJob = jobRuncount++;
+  const thisJob = jobRunCount++;
   console.log(`#${thisJob} started`);
-  await sleep(30 * 1000);  // 3 jobs will run at a time
-  // await sleep(3 * 1000);  // only one job will run at a time
+
+  // 3 job instances will be running at the same time, as specified by `concurrency` above
+  await sleep(30 * 1000);
+  // Comment the job processing statement above, and uncomment one of the blocks below
+
+  /*
+  // Imagine a job that takes 8 seconds. That is longer than the lockLifetime, so
+  // we'll break it into smaller chunks (or set its lockLifetime to a higher value).
+  await sleep(4 * 1000);  // 4000 < lockLifetime of 5000, so the job still has time to finish
+  await job.touch();      // tell Agenda the job is still running, which resets the lock timeout
+  await sleep(4 * 1000);  // do another chunk of work that takes less than the lockLifetime
+  */
+
+  // Only one job will run at a time because 3000 < lockLifetime
+  // await sleep(3 * 1000);
+
   console.log(`#${thisJob} finished`);
   done();
 });
