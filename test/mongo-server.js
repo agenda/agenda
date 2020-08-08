68309a9ce6f5bc0310466d9c659d1b6850ff457f
Patrick Fowler <pfowler@cs.stanford.edu>
2020-08-07 18:21:35 -0700

    Fix Linting Errors in the Test Directory * Add test-specific `xo-config` file * Remove extra declarations and rule exceptions from tests * formatting changes from `xo --fix`

--- test/mongo-server.js
+++ test/mongo-server.js
@@ -4,15 +4,13 @@ const {MongoMemoryServer} = require('mongodb-memory-server');
 let connectionString;
 
 let mongoServer;
-// eslint-disable-next-line no-undef
-beforeEach(async() => {
-  mongoServer = new MongoMemoryServer({});
-  connectionString = await mongoServer.getConnectionString();
+beforeEach(async () => {
+	mongoServer = new MongoMemoryServer({});
+	connectionString = await mongoServer.getConnectionString();
 });
 
-// eslint-disable-next-line no-undef
-afterEach(async() => {
-  await mongoServer.stop();
+afterEach(async () => {
+	await mongoServer.stop();
 });
 
 exports.getConnectionString = () => connectionString;
--- test/retry.js
+++ test/retry.js
@@ -1,86 +1,76 @@
-/* globals describe, it, beforeEach, afterEach, before */
 'use strict';
 const delay = require('delay');
 const {MongoClient} = require('mongodb');
 const Agenda = require('..');
 const mongoServer = require('./mongo-server');
-const processEvery = require('../lib/agenda/process-every');
 
 let mongoCfg;
 beforeEach(() => {
-  mongoCfg = mongoServer.getConnectionString();
+	mongoCfg = mongoServer.getConnectionString();
 });
-
-const agendaDatabase = 'agenda-test';
 
 // Create agenda instances
 let agenda = null;
-let mongoDb = null;
 let mongoClient = null;
 
 const jobType = 'do work';
 const jobProcessor = () => {};
 
-describe.only('Retry', () => {
-  beforeEach(async () => {
-    agenda = new Agenda({
-      db: {
-        address: mongoCfg
-      },
-      processEvery: 100,
-    });
+describe('Retry', () => {
+	beforeEach(async () => {
+		agenda = new Agenda({
+			db: {
+				address: mongoCfg
+			},
+			processEvery: 100
+		});
 
-    await agenda.start();
+		await agenda.start();
 
-    const client = await MongoClient.connect(mongoCfg);
-   
-    mongoClient = client;
-    mongoDb = client.db(agendaDatabase);
-     
-    mongoDb = mongoClient.db(agendaDatabase);
+		mongoClient = await MongoClient.connect(mongoCfg);
 
-    await delay(50);
+		await delay(50);
 
-    agenda.define('someJob', jobProcessor);
-    agenda.define('send email', jobProcessor);
-    agenda.define('some job', jobProcessor);
-    agenda.define(jobType, jobProcessor);
-  });
+		agenda.define('someJob', jobProcessor);
+		agenda.define('send email', jobProcessor);
+		agenda.define('some job', jobProcessor);
+		agenda.define(jobType, jobProcessor);
+	});
 
-  afterEach(async() => {
-    await delay(50);
-    await agenda.stop();
-    await mongoClient.close();
-    await agenda._db.close();
-  });
+	afterEach(async () => {
+		await delay(50);
+		await agenda.stop();
+		await mongoClient.close();
+		await agenda._db.close();
+	});
 
-  it('should retry a job', async() => {
-    let shouldFail = true;
+	it('should retry a job', async () => {
+		let shouldFail = true;
 
-    agenda.define('a job', (job, done) => {
-      if (shouldFail) {
-        shouldFail = false;
-        return done(new Error('test failure'));
-      }
+		agenda.define('a job', (job, done) => {
+			if (shouldFail) {
+				shouldFail = false;
+				return done(new Error('test failure'));
+			}
 
-      done();
-    });
+			done();
+		});
 
-    agenda.on('fail:a job', (err, job) => {
-      if (err) {
-        // Do nothing as this is expected to fail.
-      }
+		agenda.on('fail:a job', (err, job) => {
+			if (err) {
+				// Do nothing as this is expected to fail.
+			}
 
-      job.schedule('now').save();
-    });
+			job.schedule('now').save();
+		});
 
-    const successPromise = new Promise(resolve =>
-      agenda.on('success:a job', resolve)
-    );
+		const successPromise = new Promise(resolve =>
+			agenda.on('success:a job', resolve)
+		);
 
-    await agenda.now('a job');
+		await agenda.now('a job');
 
-    await agenda.start();
-    await successPromise;
-  });
+		await agenda.start();
+		await successPromise;
+	});
 });
