78a08cae9d7f15a6f80e3a907a3a131cab1d28d2
Surya Kanta Naik <suryanaik@users.noreply.github.com>
2020-12-29 01:42:41 +0530

    Run job between start and end date + run job every x days (#361)
    * Modified job.repeatEvery to include startDate and skipDate in the option to achieve the following scenarios:
    - Every x days at 8:00 AM CST starting 30-Aug-2016
    - Daily at 8:00 AM CST starting 30-Aug-2016
    - Every Wed and Fri at 8:00 AM CST Starting 30-Aug-2016
    
    * Fixed failing test
    
    * Added endDate options
    
    * Fixed missing endDate
    
    * Changed the indentation to 2 from 4
    
    * Update agenda.js
    
    * Update agenda.js
    
    * added tests and typescript
    
    Co-authored-by: Surya Naik <surya.naik@saviance.com>
    Co-authored-by: Leonard Lin <leonard@linhome.ch>

--- lib/job/compute-next-run-at.ts
+++ lib/job/compute-next-run-at.ts
@@ -34,40 +34,68 @@ export const computeNextRunAt = function(this: Job) {
    */
   const computeFromInterval = () => {
     debug('[%s:%s] computing next run via interval [%s]', this.attrs.name, this.attrs._id, interval);
-    let lastRun = this.attrs.lastRunAt || new Date();
-    lastRun = dateForTimezone(lastRun);
-    const cronOptions: any = { currentDate: lastRun.toDate() };
+    const dateNow = new Date();
+    let lastRun: Date = this.attrs.lastRunAt || dateNow;
+    let { startDate, endDate, skipDays } = this.attrs;
+    lastRun = dateForTimezone(lastRun).toDate();
+    const cronOptions: any = { currentDate: lastRun };
     if (timezone) {
       cronOptions.tz = timezone;
     }
 
     try {
       let cronTime = parser.parseExpression(interval, cronOptions);
-      let nextDate = cronTime.next().toDate();
-      if (nextDate.valueOf() === lastRun.valueOf() || nextDate.valueOf() <= previousNextRunAt.valueOf()) {
+      let nextDate: Date | null = cronTime.next().toDate();
+      if (nextDate.getTime() === lastRun.getTime() || nextDate.getTime() <= previousNextRunAt.getTime()) {
         // Handle cronTime giving back the same date for the next run time
-        cronOptions.currentDate = new Date((lastRun as moment.Moment).valueOf() + 1000);
+        cronOptions.currentDate = new Date(lastRun.getTime() + 1000);
         cronTime = parser.parseExpression(interval, cronOptions);
         nextDate = cronTime.next().toDate();
       }
 
+      // If start date is present, check if the nextDate should be larger or equal to startDate. If not set startDate as nextDate
+      if (startDate !== null) {
+        startDate = moment.tz(moment(startDate).format('YYYY-MM-DD'), timezone).toDate();
+        if (startDate > nextDate) {
+          cronOptions.currentDate = startDate;
+          cronTime = parser.parseExpression(interval, cronOptions);
+          nextDate = cronTime.next().toDate();
+        }
+      }
+
+      // If job has run in the past and skipDays is not null, add skipDays to nextDate
+      if ((dateNow > lastRun) && skipDays !== null) {
+        try {
+          nextDate = new Date(nextDate.getTime() + (humanInterval(skipDays) ?? 0));
+        } catch {}
+      }
+
+      // If endDate is less than the nextDate, set nextDate to null to stop the job from running further
+      if (endDate !== null) {
+        const endDateDate: Date = moment.tz(moment(endDate).format('YYYY-MM-DD'), timezone).toDate();
+        if (nextDate > endDateDate) {
+          nextDate = null;
+        }
+      }
+
       this.attrs.nextRunAt = nextDate;
-      debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, new Date(this.attrs.nextRunAt).toISOString());
+      debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, this.attrs.nextRunAt.toISOString());
     // Either `xo` linter or Node.js 8 stumble on this line if it isn't just ignored
     } catch {
+      debug('[%s:%s] failed nextRunAt based on interval [%s]', this.attrs.name, this.attrs._id, interval);
       // Nope, humanInterval then!
       try {
         if (!this.attrs.lastRunAt && humanInterval(interval)) {
-          this.attrs.nextRunAt = lastRun.valueOf();
-          debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, new Date(this.attrs.nextRunAt).toISOString());
+          this.attrs.nextRunAt = lastRun;
+          debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, this.attrs.nextRunAt.toISOString());
         } else {
-          this.attrs.nextRunAt = (lastRun as moment.Moment).valueOf() + (humanInterval(interval) ?? 0);
-          debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, new Date(this.attrs.nextRunAt).toISOString());
+          this.attrs.nextRunAt = new Date(lastRun.getTime() + (humanInterval(interval) ?? 0));
+          debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, this.attrs.nextRunAt.toISOString());
         }
       // Either `xo` linter or Node.js 8 stumble on this line if it isn't just ignored
       } catch {}
     } finally {
-      if (Number.isNaN(this.attrs.nextRunAt)) {
+      if (Number.isNaN(this.attrs.nextRunAt.getTime())) {
         this.attrs.nextRunAt = undefined;
         debug('[%s:%s] failed to calculate nextRunAt due to invalid repeat interval', this.attrs.name, this.attrs._id);
         this.fail('failed to calculate nextRunAt due to invalid repeat interval');
@@ -80,15 +108,15 @@ export const computeNextRunAt = function(this: Job) {
    */
   const computeFromRepeatAt = () => {
     const lastRun = this.attrs.lastRunAt || new Date();
-    const nextDate = date(repeatAt).valueOf();
+    const nextDate: Date = date(repeatAt);
 
     // If you do not specify offset date for below test it will fail for ms
     const offset = Date.now();
-    if (offset === date(repeatAt, offset).valueOf()) {
+    if (offset === date(repeatAt, offset).getTime()) {
       this.attrs.nextRunAt = undefined;
       debug('[%s:%s] failed to calculate repeatAt due to invalid format', this.attrs.name, this.attrs._id);
       this.fail('failed to calculate repeatAt time due to invalid format');
-    } else if (nextDate.valueOf() === lastRun.valueOf()) {
+    } else if (nextDate.getTime() === lastRun.getTime()) {
       this.attrs.nextRunAt = date('tomorrow at ', repeatAt);
       debug('[%s:%s] nextRunAt set to [%s]', this.attrs.name, this.attrs._id, this.attrs.nextRunAt.toISOString());
     } else {
