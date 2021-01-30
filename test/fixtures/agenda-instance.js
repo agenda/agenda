071e0b91670857c3a1163b561369b33efdbe9eb7
Haris Sulaiman <harisvsulaiman@gmail.com>
2021-01-30 11:42:29 +0530

    format tests
    Signed-off-by: Haris Sulaiman <harisvsulaiman@gmail.com>

--- test/fixtures/agenda-instance.js
+++ test/fixtures/agenda-instance.js
@@ -1,5 +1,5 @@
-const { Agenda } = require('../../dist');
-const addTests = require('./add-tests');
+const { Agenda } = require("../../dist");
+const addTests = require("./add-tests");
 
 const connString = process.argv[2];
 const tests = process.argv.slice(3);
@@ -7,9 +7,9 @@ const tests = process.argv.slice(3);
 const agenda = new Agenda(
   {
     db: {
-      address: connString
+      address: connString,
     },
-    processEvery: 100
+    processEvery: 100,
   },
   async () => {
     tests.forEach((test) => {
@@ -19,15 +19,15 @@ const agenda = new Agenda(
     await agenda.start();
 
     // Ensure we can shut down the process from tests
-    process.on('message', (message) => {
-      if (message === 'exit') {
+    process.on("message", (message) => {
+      if (message === "exit") {
         process.exit(0);
       }
     });
 
     // Send default message of "notRan" after 400ms
     setTimeout(() => {
-      process.send('notRan');
+      process.send("notRan");
       // eslint-disable-next-line unicorn/no-process-exit
       process.exit(0);
     }, 400);
