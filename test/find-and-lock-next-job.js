c49255ddce9df71d430aae090309aa8c45e1397f
vasyl <vasyl@flash-fx.com.au>
2021-02-06 12:03:23 +1100

    Find stuck jobs and rerun them.

--- test/find-and-lock-next-job.js
+++ test/find-and-lock-next-job.js
@@ -66,19 +66,55 @@ describe("find-and-lock-next-job", () => {
     await agenda._db.close();
   });
 
-  it("should find jobs with and without lockedAt property", async () => {
+  it("should find jobs without lockedAt property", async () => {
+    const collection = await mongoDb.collection("agendaJobs");
     const job = await agenda.create(jobType, {}).save();
     expect(job.attrs.lockedAt).to.equal(undefined);
     // The above line does not add `lockedAt` to DB. Nevertheless, let's delete it just in case.
-    const { lastErrorObject, value } = await mongoDb
-      .collection("agendaJobs")
-      .findOneAndUpdate({ name: jobType }, { $unset: { lockedAt: "" } }); // deleting the property
+    const { lastErrorObject } = await collection.findOneAndUpdate(
+      { name: jobType },
+      { $unset: { lockedAt: "" } }
+    ); // deleting the property
     expect(lastErrorObject.updatedExisting).to.equal(true);
-    expect(value.lockedAt).to.equal(undefined);
-    expect(value.nextRunAt.getTime()).to.be.lessThan(Date.now() + 1000);
+    const rawJob = await collection.findOne({ name: jobType });
+    expect(rawJob.lockedAt).to.equal(undefined);
+    expect(rawJob.nextRunAt.getTime()).to.be.lessThan(Date.now() + 1000);
 
     agenda.processEvery(100);
+    let processed = 0;
+    const successPromise = new Promise((resolve) => {
+      processed += 1;
+      agenda.on(`success:${jobType}`, resolve);
+    });
 
+    await agenda.start();
+    await successPromise;
+
+    expect(processed).to.equal(1);
+  });
+
+  it("should find and rerun stuck jobs (with long ago lockedAt property)", async () => {
+    const collection = await mongoDb.collection("agendaJobs");
+    const job = await agenda.create(jobType, {}).save();
+    expect(job.attrs.lockedAt).to.equal(undefined);
+    // The above line does not add `lockedAt` to DB. The below simulates a stuck job.
+    const { lastErrorObject } = await collection.findOneAndUpdate(
+      { name: jobType },
+      {
+        $unset: { nextRunAt: "" },
+        $set: {
+          lockedAt: new Date(Date.now() - agenda._defaultLockLifetime - 1000),
+        },
+      }
+    ); //
+    expect(lastErrorObject.updatedExisting).to.equal(true);
+    const rawJob = await collection.findOne({ name: jobType });
+    expect(rawJob.nextRunAt).to.equal(undefined);
+    expect(rawJob.lockedAt.getTime()).to.be.lessThan(
+      Date.now() - agenda._defaultLockLifetime
+    );
+
+    agenda.processEvery(100);
     let processed = 0;
     const successPromise = new Promise((resolve) => {
       processed += 1;
