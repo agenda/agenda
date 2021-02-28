9952b8f85163ff589ef04f2bbd9ef98bbb7a3a09
Jonas Strassel <jonas.strassel@mobilehead.com>
2021-02-28 14:32:52 +0100

    fix: export more things from top-level

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,8 @@
-export { Agenda, AgendaConfig } from "./agenda";
+export * from "./agenda";
+export * from "./job";
 
 export { DefineOptions, JobPriority, Processor } from "./agenda/define";
 export { JobOptions } from "./job/repeat-every";
+
+import { Agenda } from "./agenda";
+export default Agenda;
