ffb0c6fec0fbe562f072e0359fc343a936d74c54
simon <s.tretter@gmail.com>
2022-01-10 12:43:33 +0100

    perf: improve perforamnce for shard updates by adding name to update query

--- src/JobDbRepository.ts
+++ src/JobDbRepository.ts
@@ -230,9 +230,7 @@ export class JobDbRepository {
 		// We have a result from the above calls
 		if (res) {
 			// Grab ID and nextRunAt from MongoDB and store it as an attribute on Job
-			// eslint-disable-next-line no-param-reassign
 			job.attrs._id = res._id;
-			// eslint-disable-next-line no-param-reassign
 			job.attrs.nextRunAt = res.nextRunAt;
 
 			// check if we should process the job immediately
@@ -244,20 +242,24 @@ export class JobDbRepository {
 	}
 
 	async saveJobState(job: Job<any>): Promise<void> {
+		const id = job.attrs._id;
+		const $set = {
+			lockedAt: (job.attrs.lockedAt && new Date(job.attrs.lockedAt)) || undefined,
+			nextRunAt: (job.attrs.nextRunAt && new Date(job.attrs.nextRunAt)) || undefined,
+			lastRunAt: (job.attrs.lastRunAt && new Date(job.attrs.lastRunAt)) || undefined,
+			progress: job.attrs.progress,
+			failReason: job.attrs.failReason,
+			failCount: job.attrs.failCount,
+			failedAt: job.attrs.failedAt && new Date(job.attrs.failedAt),
+			lastFinishedAt: (job.attrs.lastFinishedAt && new Date(job.attrs.lastFinishedAt)) || undefined
+		};
+
+		log('[job %s] save job state: \n%O', id, $set);
+
 		await this.collection.updateOne(
-			{ _id: job.attrs._id },
+			{ _id: id, name: job.attrs.name },
 			{
-				$set: {
-					lockedAt: (job.attrs.lockedAt && new Date(job.attrs.lockedAt)) || undefined,
-					nextRunAt: (job.attrs.nextRunAt && new Date(job.attrs.nextRunAt)) || undefined,
-					lastRunAt: (job.attrs.lastRunAt && new Date(job.attrs.lastRunAt)) || undefined,
-					progress: job.attrs.progress,
-					failReason: job.attrs.failReason,
-					failCount: job.attrs.failCount,
-					failedAt: job.attrs.failedAt && new Date(job.attrs.failedAt),
-					lastFinishedAt:
-						(job.attrs.lastFinishedAt && new Date(job.attrs.lastFinishedAt)) || undefined
-				}
+				$set
 			}
 		);
 	}
