9d20a0463caac1b8f0b554ba4f053adbdbcb4f3e
simon <s.tretter@gmail.com>
2022-08-02 14:24:43 +0200

    test: node 14 compatibility for assert

--- test/job.test.ts
+++ test/job.test.ts
@@ -2,7 +2,7 @@
 import * as path from 'path';
 import * as cp from 'child_process';
 import { expect } from 'chai';
-import * as assert from 'node:assert/strict';
+import * as assert from 'node:assert';
 import { DateTime } from 'luxon';
 import { Db } from 'mongodb';
 
