73dc82efe5333bd36473a757d801e42c62e786fb
Haris Sulaiman <haris@cloodot.com>
2022-11-09 12:30:49 +0530

    Remove agenda files
    Signed-off-by: Haris Sulaiman <haris@cloodot.com>

--- test/imports/require-es.js
+++
@@ -1,5 +0,0 @@
-const assert = require("assert");
-
-const Agenda1 = require("../../es").default;
-const { Agenda: Agenda2 } = require("../../es");
-assert.strictEqual(Agenda1, Agenda2);
--- test/imports/require.js
+++
@@ -1,5 +0,0 @@
-const assert = require("assert");
-
-const Agenda1 = require("../..");
-const { Agenda: Agenda2 } = require("../..");
-assert.strictEqual(Agenda1, Agenda2);
